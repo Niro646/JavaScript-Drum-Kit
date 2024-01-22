f22288b863cf9cda171d879df09e73515d802029
Niro646 <nir602@gmail.com>
2024-01-22 17:25:32 +0200

    touch update

--- drumsCode.js
+++ drumsCode.js
@@ -1,10 +1,8 @@
-console.log("comment 0");
+window.addEventListener("keydown", playSound); // when a key is pressed
 
 function removeTransition(e) {
-  console.log(e);
   //if (e.propertyName !== "transform") return; //  checks if represents the name of the CSS property that has completed its transition.
   e.target.classList.remove("playing"); // e.target refers to the pressed element
-  console.log("comment 1");
 }
 
 function playSound(e) {
@@ -15,13 +13,26 @@ function playSound(e) {
   key.classList.add("playing");
   audio.currentTime = 0;
   audio.play();
-  console.log("comment 2");
+}
+
+function playSoundTouch(e) {
+  const touchAudio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const touchKey = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+
+  key.classList.add("playing");
+  audio.currentTime = 0;
+  audio.play();
 }
 
 const keys = Array.from(document.querySelectorAll(".key")); // creates array with all keys
 keys.forEach(
   (key) => key.addEventListener("transitionend", removeTransition) // when a css element has changed
 );
-console.log("comment 4");
-window.addEventListener("keydown", playSound); // when a key is pressed
-console.log("comment 5");
+
+keys.forEach((key) => {
+  key.addEventListener("touchstart", (e) => {
+    e.preventDefault(); // Prevent default touch behavior (e.g., scrolling)
+    playSoundTouch(key);
+  });
+});
